dd426af6ec2a37a241d8d620b8b1317710974384
Eugene Kilachkoff <ekilachkoff@gmail.com>
2010-12-04 05:31:52 +0300

    sensor is basically working

--- main.c
+++ main.c
@@ -20,54 +20,60 @@
 
 NOINIT uint8_t mcusr_mirror;
 
-PROGMEM char usbHidReportDescriptor[31] = {
+#if 0
+PROGMEM char usbHidReportDescriptor[36] = {
     0x06, 0x00, 0xff,              // USAGE_PAGE (Vendor Defined Page 1)
     0x09, 0x01,                    // USAGE (Vendor Usage 1)
     0xa1, 0x01,                    // COLLECTION (Application)
     0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x27, 0xff, 0xff, 0xff, 0x7f,  //   LOGICAL_MAXIMUM (2147483647)
+    0x27, 0xff, 0xff, 0x00, 0x00,  //   LOGICAL_MAXIMUM (65535)
     0x35, 0x00,                    //   PHYSICAL_MINIMUM (0)
-    0x47, 0x40, 0x42, 0x0f, 0x00,  //   PHYSICAL_MAXIMUM (1000000)
+    0x47, 0xff, 0xff, 0x00, 0x00,  //   PHYSICAL_MAXIMUM (65535)
+    0x55, 0x0E,                    //   UNIT_EXPONENT (-2)
     0x67, 0xe1, 0x00, 0x00, 0x01,  //   UNIT (SI Lin:0x10000e1)
-    0x75, 0x20,                    //   REPORT_SIZE (32)
-    0x95, 0x01,                    //   REPORT_COUNT (1)
+    0x75, 0x10,                    //   REPORT_SIZE (16)
+    0x95, 0x02,                    //   REPORT_COUNT (4)
+    0x82, 0x62, 0x01,              // INPUT (Data,Var,Abs,NPrf,Null,Buf)
+    0xc0                           // END_COLLECTION
+};
+#endif
+
+/* we will use feature reports to transfer arbitrary data */
+PROGMEM char usbHidReportDescriptor[24] = {
+    0x06, 0x00, 0xff,              // USAGE_PAGE (Generic Desktop)
+    0x09, 0x01,                    // USAGE (Vendor Usage 1)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x27, 0xff, 0xff, 0x00, 0x00,  //   LOGICAL_MAXIMUM (65535)
+    0x75, 0x10,                    //   REPORT_SIZE (16)
+    0x95, 0x02,                    //   REPORT_COUNT (4)
+    0x09, 0x00,                    //   USAGE(Undefined)
+    0xb2, 0x02, 0x01,              //   FEATURE(Data,Var,Abs,Buf)
     0xc0                           // END_COLLECTION
 };
 
-
 typedef struct usbHidReport
 {
-	uint32_t value;
+	uint16_t value;
+	uint16_t fp_value;
 }report_type;
 
 NOINIT static report_type report;
-NOINIT static uint8_t idleRate;
 
 USB_PUBLIC usbMsgLen_t usbFunctionSetup(uchar data[8])
 {
 	usbRequest_t    *rq = (void *)data;
 
-	/* The following requests are never used. But since they are required by
-	 * the specification, we implement them in this example.
-	 */
 	switch ( rq->bmRequestType & USBRQ_TYPE_MASK )
 	{
 	case USBRQ_TYPE_CLASS:
-		//DBG1(0x50, &rq->bRequest, 1);   /* debug output: print our request */
 		switch ( rq->bRequest )
 		{
 		case USBRQ_HID_GET_REPORT:
 			/* wValue: ReportType (highbyte), ReportID (lowbyte) */
 			/* we only have one report type, so don't look at wValue */
 			usbMsgPtr = (void *)&report;
-			report.value++; /* simply count requests for now */
 			return sizeof(report);
-		case USBRQ_HID_GET_IDLE:
-			usbMsgPtr = &idleRate;
-			return 1;
-		case USBRQ_HID_SET_IDLE:
-			idleRate = rq->wValue.bytes[1];
-			return 0;
 		}
 		break;
 	default: /* no vendor specific requests implemented */
@@ -106,6 +112,8 @@ INIT_FUNC_3 void early_init(void)
 INIT_FUNC_8 void late_init(void)
 {
 	usbInit();
+	sampler_init();
+	report.value = 0;
 }
 
 #if defined(__GNUC__) && defined(__AVR__)
@@ -121,12 +129,22 @@ int main(void)
 
 	sei();
 
+	sampler_start();
 	for(;;)
 	{
 		usbPoll();
+		if ( sampler_poll() )
+		{
+			const uint16_t fp_raw = sampler_get_sample();
+			report.fp_value = fp_reciprocal(fp_raw);
+			report.value = fp_to_uint16(report.fp_value);
+			sampler_start();
+		}
+#if 0
 		if ( usbInterruptIsReady() )
 		{
 			usbSetInterrupt(0/*TODO*/, 0/*TODO*/);
 		}
+#endif
 	}
 }
